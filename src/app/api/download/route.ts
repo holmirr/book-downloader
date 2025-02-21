4260808fcbba754131a804bed21715efde95bc0a
holmirr707 <tohroku707@gmail.com>
2025-02-21 13:02:32 +0900

    aa

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -41,6 +41,7 @@ export async function GET(request: Request): Promise<Response> {
       }
     },
     cancel() {
+      console.log("readable stream cancel block")
       abortController.abort();
     }
   });
