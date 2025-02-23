22765608f8149cd8e0793def91fff8d82f570abe
holmirr707 <tohroku707@gmail.com>
2025-02-23 15:47:20 +0900

    ユーザー名→メールアドレス

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -21,7 +21,7 @@ export default function Login() {
             <User className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
             <input
               type="text"
-              placeholder="ユーザー名"
+              placeholder="メールアドレス"
               disabled={isPending}
               name="email"
               required
